395c8f6b7ad570d6cb8600dcc4b13aafd55de5ee
GilbertoHernandez150 <ghernandezds@gmail.com>
2026-02-06 17:24:57 -0400

    .

--- Blazor/Diario de videojuegos/Models/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-// Models/LoginModel.cs
-namespace DiarioVideojuegos.Models
-{
-    public class LoginModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
